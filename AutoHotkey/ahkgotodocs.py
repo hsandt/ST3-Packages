767b06de07253d31ee46a9468cd06a806cfd8e92
huulong <n.huu.long@gmail.com>
2016-04-26 15:06:37 +0200

    [AHK] Upgraded package

--- AutoHotkey/ahkgotodocs.py
+++ AutoHotkey/ahkgotodocs.py
@@ -10,17 +10,20 @@ class ahkgotodocs(sublime_plugin.TextCommand):
 
 			# If no selection, find word contained by whitespace
 			if len(word) == 0:
-				word = self.view.substr(self.view.word(region))
+				region = self.view.word(region)
+				if region.a != 0:
+					region.a = region.a - 1
+				word = self.view.substr(region)
+				if word[0] != '#':
+					word = word[1:]
 
 			if len(word) != 0:
-				
 				# Remove non-alpha characters
 				wordalphaonly = ''
 				for ch in word:
-					if(ch.isalpha()):
+					if ch.isalpha() == True or ch == '#':
 						wordalphaonly += ch
-				word = wordalphaonly
-
+				word = wordalphaonly.replace('#', '_')
 				sublime.status_message("AHKGOTODOCS Loading Documentation For: " + word)
 				self.ahkgotodocs_hh(word)
 				# self.ahkgotodocs_url(keyword)
