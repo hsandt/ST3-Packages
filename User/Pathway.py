124646d2eb71b82c64ef52cee160999d14f78855
hsandt <hs@fuwar.in>
2017-08-26 17:49:29 +0200

    [GIT] Ignore Haxe cache folder, added path for Linux in GitGutter settings [BUILD] Added C++11 Single File build, improved cmake build command [KEYBOARD] Linux: added lower case shortcut [PACKAGES] Installed Crystal and Haxe [PATH] Added HOME path (doesn't work on Linux with simple build commands)

--- User/Pathway.py
+++ User/Pathway.py
@@ -4,15 +4,18 @@ import os
 
 SYSTEM = '/usr/bin:/bin:/usr/sbin:/sbin'
 LOCAL = '/usr/local/bin:/usr/local/sbin'
-# HOME = os.environ['HOME']  # uncomment if you want to use binaries from your home
+HOME = os.environ['HOME']  # uncomment if you want to use binaries from your home
 # RVM = HOME + '/.rvm/bin:'
 
 # Sublime's default path is
 # /usr/bin:/bin:/usr/sbin:/sbin
 # but we rebuild the path from zero to avoid redundancy
-os.environ['PATH'] = SYSTEM
+os.environ['PATH'] = ''
+os.environ['PATH'] += HOME + '/bin'
 os.environ['PATH'] += ':'
 os.environ['PATH'] += LOCAL
+os.environ['PATH'] += ':'
+os.environ['PATH'] += SYSTEM
 # os.environ['PATH'] += RVM
 
 print('PATH = ' + os.environ['PATH'])
