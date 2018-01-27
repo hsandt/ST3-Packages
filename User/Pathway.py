535a683a4bd2ae7b03a45ac16b60e8c532ab2605
huulong <n.huu.long@gmail.com>
2018-01-27 16:26:34 +0100

    Merge branch 'master' into mac

--- User/Pathway.py
+++ User/Pathway.py
@@ -4,15 +4,21 @@ import os
 
 SYSTEM = '/usr/bin:/bin:/usr/sbin:/sbin'
 LOCAL = '/usr/local/bin:/usr/local/sbin'
-# HOME = os.environ['HOME']  # uncomment if you want to use binaries from your home
-# RVM = HOME + '/.rvm/bin:'
+HOME = os.environ['HOME']  # uncomment if you want to use binaries from your home
+CARGO = HOME + '/.cargo/bin'
+# RVM = HOME + '/.rvm/bin'
 
 # Sublime's default path is
 # /usr/bin:/bin:/usr/sbin:/sbin
 # but we rebuild the path from zero to avoid redundancy
-os.environ['PATH'] = SYSTEM
+os.environ['PATH'] = ''
+os.environ['PATH'] += HOME + '/bin'
 os.environ['PATH'] += ':'
 os.environ['PATH'] += LOCAL
+os.environ['PATH'] += ':'
+os.environ['PATH'] += CARGO
+os.environ['PATH'] += ':'
+os.environ['PATH'] += SYSTEM
 # os.environ['PATH'] += RVM
 
 print('PATH = ' + os.environ['PATH'])
