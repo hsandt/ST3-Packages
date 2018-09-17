396db5c2a82c1f451045ab06ed7dff5fe024fbe5
huulong <n.huu.long@gmail.com>
2018-09-17 17:57:43 +0200

    [PATH] Generic paths for Pathway.py

--- User/Pathway.py
+++ User/Pathway.py
@@ -4,24 +4,22 @@ import os
 
 SYSTEM = '/usr/bin:/bin:/usr/sbin:/sbin'
 LOCAL = '/usr/local/bin:/usr/local/sbin'
-HOME = os.environ['HOME']  # uncomment if you want to use binaries from your home
-CARGO = HOME + '/.cargo/bin'
-LUA = HOME + '/Developer/hererocks/lua5.3/bin'
-# RVM = HOME + '/.rvm/bin'
+HOME = os.environ.get('HOME', None)
+if HOME is not None:
+    HOME_BIN = HOME + '/bin'
+    CARGO_BIN = HOME + '/.cargo/bin'
+    LUA_BIN = HOME + '/Developer/hererocks/lua5.3/bin'
+    RVM_BIN = HOME + '/.rvm/bin'
+else:
+    HOME_BIN = None
+    CARGO_BIN = None
+    LUA_BIN = None
+    RVM_BIN = None
 
 # Sublime's default path is
 # /usr/bin:/bin:/usr/sbin:/sbin
-# but we rebuild the path from zero to avoid redundancy
-os.environ['PATH'] = ''
-os.environ['PATH'] += HOME + '/bin'
-os.environ['PATH'] += ':'
-os.environ['PATH'] += LOCAL
-os.environ['PATH'] += ':'
-os.environ['PATH'] += CARGO
-os.environ['PATH'] += ':'
-os.environ['PATH'] += SYSTEM
-os.environ['PATH'] += ':'
-os.environ['PATH'] += LUA
-# os.environ['PATH'] += RVM
+# but we rebuild the path from zero to avoid redundancy on multiple reload
+paths = [path for path in (HOME_BIN, LOCAL, CARGO_BIN, LUA_BIN, RVM_BIN, SYSTEM) if path is not None]
+os.environ['PATH'] = ':'.join(paths)
 
 print('PATH = ' + os.environ['PATH'])
