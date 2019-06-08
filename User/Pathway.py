0d430c2868e070fe8ded50f77c975ca61ad3487f
huulong <n.huu.long@gmail.com>
2019-06-08 20:17:50 +0100

    [PATHWAY] Added HOME_LOCAL_BIN

--- User/Pathway.py
+++ User/Pathway.py
@@ -7,11 +7,13 @@ LOCAL = '/usr/local/bin:/usr/local/sbin'
 HOME = os.environ.get('HOME', None)
 if HOME is not None:
     HOME_BIN = HOME + '/bin'
+    HOME_LOCAL_BIN = HOME + '/.local/bin'
     CARGO_BIN = HOME + '/.cargo/bin'
     LUA_BIN = HOME + '/Developer/hererocks/lua5.3/bin'
     RVM_BIN = HOME + '/.rvm/bin'
 else:
     HOME_BIN = None
+    HOME_LOCAL_BIN = None
     CARGO_BIN = None
     LUA_BIN = None
     RVM_BIN = None
@@ -19,7 +21,7 @@ else:
 # Sublime's default path is
 # /usr/bin:/bin:/usr/sbin:/sbin
 # but we rebuild the path from zero to avoid redundancy on multiple reload
-paths = [path for path in (HOME_BIN, LOCAL, CARGO_BIN, LUA_BIN, RVM_BIN, SYSTEM) if path is not None]
+paths = [path for path in (HOME_BIN, HOME_LOCAL_BIN, LOCAL, CARGO_BIN, LUA_BIN, RVM_BIN, SYSTEM) if path is not None]
 os.environ['PATH'] = ':'.join(paths)
 
 print('PATH = ' + os.environ['PATH'])
